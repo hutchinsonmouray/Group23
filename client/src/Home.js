421a3fe6be526aad4ae986e27eb621d0ae78f5f4
hutchinsonmouray <80347718+hutchinsonmouray@users.noreply.github.com>
2022-03-27 14:51:36 -0400

    Merge remote-tracking branch 'origin/base' into mouray

--- client/src/Home.js
+++ client/src/Home.js
@@ -5,7 +5,7 @@ import {createStore,applyMiddleware,compose} from 'redux';
 import thunk from 'redux-thunk';
 import reducers from "./reducers";
 
-import {BrowserRouter, Link, Route} from "react-router-dom"; //for multipage
+import {BrowserRouter, Link, NavLink, Route} from "react-router-dom"; //for multipage
 
 import App from './App';
 import {AppBar, Button, Container, Grid, Grow, TableRow, Typography} from "@material-ui/core";
@@ -34,20 +34,19 @@ import UploadLecture from "./UploadLecture";
     return (
         <Container maxidth={"lg"}>
             <AppBar className={classes.appBar} position = "static" color ="inherit">
-                    <button onClick={LearnSS}>
-                        <p>SAMPLE BUTTTON upload sets</p>
-                        <img className={classes.image} src = {memories} alt = "StudyStream Logo" height="60"/>
-                        {/*<img src="https://www.tutorialspoint.com/latest/inter-process-communication.png " />*/}
+                  <NavLink to={"/upload_lecture"}>
+                    <button>
+                        <p>upload sets</p>
                     </button>
+                      </NavLink>
 
-                    <Typography className={classes.heading} variant="h2" align="center">StudyStream</Typography>
-                    {/*<img className={classes.image} src = {memories} alt = "StudyStream Logo" height="60"/>*/}
+                <Typography className={classes.heading} variant="h2" align="center">StudyStream</Typography>
 
-                    <button type = "submit" name = "learn" value = "myimage" onClick={LearnSS}>
-                        <p>upload posts/lecutures</p>
-                        <img className={classes.image} src = {memories} alt = "StudyStream Logo" height="60"/>
-                        {/*<img src="https://www.tutorialspoint.com/latest/inter-process-communication.png " />*/}
-                    </button>
+                        <NavLink to={"/upload_lecture"}>
+                            <button>
+                                <p>upload posts/lecutures</p>
+                            </button>
+                        </NavLink>
             </AppBar>
             {/*//make back <Button className={classes.buttonSubmit} variant="contained" color="primary" size="small" type="submit" fullWidth>Submit Lecture</Button>*/}
             <Grow in>
@@ -71,10 +70,6 @@ import UploadLecture from "./UploadLecture";
                     </button>
                     <Grid item xs={12} sm={7}>
                             <Sets/>
-                        </Grid>
-
-                        <Grid item xs={12} sm={7}>
-                            <Form/>
                         </Grid>
                     </Grid>
             </Grow>
