3d1b74747bf2f0808acac4069c3e7100efbeff9a
Adam Horton <adam.horton@ufl.edu>
2022-04-11 13:46:33 -0400

    working parser with import

--- server/parser.js
+++ server/parser.js
@@ -1,3 +1,4 @@
-var addon = require('bindings')('parser');
+import bindings from 'bindings'
+var addon = bindings('parser')
 
 console.log(addon.parseIntoCardsFromAudio("Also Keyword good definition awesome period")); // 'world'
