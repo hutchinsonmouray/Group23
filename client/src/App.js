2755f7f8cf5d9034fcab196aa50cac87964e63c0
Adam Horton <adam.horton@ufl.edu>
2022-04-18 00:17:12 -0400

    Working form submission

--- client/src/App.js
+++ client/src/App.js
@@ -4,119 +4,55 @@ import useStyles from './styles';
 import {Container, AppBar, Typography, Grow, Grid, Button, Paper, TextField, Checkbox} from '@material-ui/core';
 import {NavLink} from "react-router-dom";
 
-function ExportLecture() {
-    //runs the c++ add on that exports the file to csv;
-    localStorage.getItem("creator").
-    localStorage.getItem("title");
-    localStorage.getItem("text");
+export default class App extends React.Component{
+    state = {
+        title: null,
+        creator: null,
+        number: null,
+        lecture: null,
+    };
 
-    console.log(localStorage.getItem("creator"))
-    console.log(localStorage.getItem("title"))
-    console.log(localStorage.getItem("text"))
-
-    // localStorage.clear();
-    return null;
-}
-
-function saveLecture() {
-    localStorage.getItem("creator");
-    localStorage.getItem("title");
-    localStorage.getItem("text");
-
-    console.log(localStorage.getItem("creator"))
-    console.log(localStorage.getItem("title"))
-    console.log(localStorage.getItem("text"))
-    //runs the c++ add on to save it as a local txt file or within the class
-
-    localStorage.clear();
-    return null;
-}
-function saveSet() {
-    //runs the c++ add on;
-    localStorage.getItem("creator");
-    localStorage.getItem("title");
-    localStorage.getItem("text");
-
-    console.log(localStorage.getItem("creator"))
-    console.log(localStorage.getItem("title"))
-    console.log(localStorage.getItem("text"))
-
-    localStorage.clear();
-    return null;
-}
-
-function uploadLecture() {
-    //runs the c++ add on;
-    localStorage.getItem("title");
-    localStorage.getItem("file");
-
-    console.log(localStorage.getItem("title"))
-    console.log(localStorage.getItem("file"))
-    //runs the c++ add on to save it as a local txt file or within the class
-
-    localStorage.clear();
-    return null;
-}
-function uploadSet() {
-    //runs the c++ add on;
-    localStorage.getItem("title");
-    localStorage.getItem("file");
-
-    console.log(localStorage.getItem("title"))
-    console.log(localStorage.getItem("file"))
-
-    // add to long term data using wrapper c++
-
-    localStorage.clear();
-    return null;
-}
-
-function setPostData(type, param) {
-    localStorage.setItem(type,param);
-    // console.log(param)
-    return null;
-}
-
-//Calls both functions
-function uploadLecture1() {
-    return uploadLecture();
-}
-
-function setSetData(param) {
-    localStorage.setItem('myData',param);
-}
-function etSetData(selectedFile) {
-    localStorage.setItem('myData',selectedFile);
-};
-
-function callAPI(text) {
-    fetch("/make-cards/24", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "transcript" : text,
-            "creator" : "bob",
-            "title" : "THE_TITLE"
+    handleTitleChange(e){
+        this.setState({
+            title: e.target.value
         })
-    })
-}
+    }
 
-class App extends React.Component {
+    handleCreatorChange(e){
+        this.setState({
+            creator: e.target.value
+        })
+    }
 
-render() {
+    handleLectureChange(e){
+        this.setState({
+            lecture: e.target.value
+        })
+    }
 
-    return (
+    handleNumberChange(e){
+        this.setState({
+            number: e.target.value
+        })
+    }
+
+    callAPI() {
+        fetch("/make-cards/" + this.state.number, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "transcript" : this.state.lecture,
+                "creator" : this.state.creator,
+                "title" : this.state.title
+            })
+        })
+    }
+
+    render(){
+        return(
         <Container container justify="center" maxidth={"lg"} align="center">
-
-            {/*<div>*/}
-            {/*    /!* Logo is an actual React component *!/*/}
-            {/*    <Logo/>*/}
-            {/*</div>*/}
-
-
-
 
         <div className={"App"}>
 
@@ -127,9 +63,6 @@ render() {
                 </AppBar>
             </NavLink>
         </div>
-                {/*<button onClick={ ()=> this.setLectureData()}>Set Lecture Data</button>*/}
-                {/*<button onClick={ ()=> this.getLectureData()}>Get Lecture Data</button>*/}
-
             {/*White bubble with grid below*/}
             <Typography className='form-box' align='center'>
                 <div className='form-box-text'>
@@ -144,13 +77,12 @@ render() {
 
             <Grid className='uploadBox'>
             {/*Lecture*/}
-                    <form autoComplete="off" noValidate onSubmit={callAPI("bob")} className='form'>
-                        {/*<div className='banner-container'>*/}
+                    <form autoComplete="off" noValidate onSubmit={(e) => this.callAPI(e)} className='form'>
                         <Typography variant="h6">{'Create a Lecture'}</Typography>
-                        {/*</div>*/}
-                        <TextField name="creator" variant="outlined" label="Creator" id="creator" fullWidth onChange={(e) => setPostData("creator",e.target.value)} />
-                        <TextField name="message" variant="outlined" label="Title" id="title" fullWidth onChange={(e) =>  setPostData("title",e.target.value)}/>
-                        <TextField name="message" variant="outlined" label="Paste Lecture Here" id="pasteLecture" fullWidth onChange={(e) =>  setPostData("text",e.target.value)}/>
+                        <TextField name="creator" variant="outlined" label="Title" id="creator" fullWidth value={this.state.title} onChange={(e) => this.handleTitleChange(e)} />
+                        <TextField name="message" variant="outlined" label="Creator" id="title" fullWidth value={this.state.creator} onChange={(e) =>  this.handleCreatorChange(e)}/>
+                        <TextField name="message" variant="outlined" label="Number" id="number" fullWidth value={this.state.number} onChange={(e) =>  this.handleNumberChange(e)}/>
+                        <TextField name="message" variant="outlined" label="Paste Lecture Here" id="pasteLecture" fullWidth value={this.state.lecture} onChange={(e) =>  this.handleLectureChange(e)}/>
                         <Button variant="contained" color="default" size="large" type="submit" fullWidth>Create a Lecture</Button>
                         <div>
                             <Checkbox></Checkbox>
@@ -159,38 +91,9 @@ render() {
                            </Typography>
                         </div>
                     </form>
-                    <form autoComplete="off" noValidate onSubmit={uploadLecture()}className='form'>
-                        <Typography color = "textPrimary" variant="h6">{'Upload a Lecture'}</Typography>
-                        <TextField name="message" variant="outlined" label="Title" fullWidth onChange={(e) =>  setPostData("title",e.target.value)} />
-                        <FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData("file",{selectedFile: base64 })} />
-                        <Button variant="contained" color="default" size="large" type="submit" fullWidth>Upload Lecture</Button>
-                    </form>
-                <form autoComplete="off" noValidate onSubmit={uploadLecture1()}className='form'>
-                    <Typography color = "textPrimary" variant="h6">{'Merge two Existing Sets'}</Typography>
-                    <TextField name="message" variant="outlined" label="Title" fullWidth onChange={(e) =>  setPostData("title",e.target.value)} />
-                    <FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData("file",{selectedFile: base64 })} />
-                    <FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData("file1",{selectedFile: base64 })} />
-                    <Button variant="contained" color="default" size="large" type="submit" fullWidth>Upload Lecture</Button>
-                </form>
-
-                    <form autoComplete="off" noValidate onSubmit={saveSet()} className='form'>
-                        <Typography variant="h6">{'Create a Set'}</Typography>
-                        <TextField name="creator" variant="outlined" label="Creator" fullWidth onChange={(e) => setPostData("creator",e.target.value)} />
-                        <TextField name="message" variant="outlined" label="Title" fullWidth onChange={(e) =>  setPostData("title",e.target.value)} />
-                        <TextField name="message" variant="outlined" label="Paste Lecture Here" fullWidth onChange={(e) =>  setPostData("text",e.target.value)} />
-                        <Button variant="contained" color="default" size="large" type="submit" fullWidth>Create a Set</Button>
-                    </form>
-
-                    <form autoComplete="off" noValidate onSubmit={uploadSet()} className='form'>
-                        <Typography variant="h6">{'Upload a Set'}</Typography>
-                        <TextField name="message" variant="outlined" label="Title" fullWidth onChange={(e) =>  setPostData("title",e.target.value)} />
-                        <FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData("file",{selectedFile: base64 })} />
-                        <Button variant="contained" color="default" size="large" type="submit" fullWidth>Submit Set</Button>
-                    </form>
-        </Grid>
+                    </Grid>
         </div>
             </Container>
         );
     }
 }
-export default App;
